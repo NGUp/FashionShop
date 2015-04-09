ba661f001c13f3f58cd08a07b47c6d1c3627ee44
vhnam <vhnam2504@gmail.com>
2015-04-09 23:37:40 +0700

    restructure project and add Unit Tests.

---
+++ FashionShop/Controllers/IndexController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FashionShop.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Index/About
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ FashionShop/Views/Index/About.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Main.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Title" ContentPlaceHolderID="TitleContent" runat="server">
+	About
+</asp:Content>
+
+<asp:Content ID="Main" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>About</h2>
+
+</asp:Content>
