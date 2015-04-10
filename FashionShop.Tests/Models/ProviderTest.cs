cef5ecc5dc119d5aa30f5f733056f2f2e0455b1e
vhnam <vhnam2504@gmail.com>
2015-04-10 20:59:02 +0700

    can C.R.U.D with Provider and testing Provider.

--- FashionShop.Tests/Models/ProviderTest.cs
+++ FashionShop.Tests/Models/ProviderTest.cs
@@ -11,26 +11,90 @@ namespace FashionShop.Tests
     [TestClass()]
     public class ProviderTest
     {
+        //[TestMethod]
+        //public void ConnectionString()
+        //{
+        //    Provider provider = new Provider();
+
+        //    Assert.AreEqual(provider.openConnection().ConnectionString, @"Server=.\SQLEXPRESS;Database=FashionShop;Integrated Security=SSPI;");
+        //}
+
+        //[TestMethod]
+        //public void TestConnection()
+        //{
+        //    Provider provider = new Provider();
+
+        //    SqlConnection connection = provider.openConnection();
+
+        //    connection.Open();
+
+        //    Assert.AreEqual(connection.State, ConnectionState.Open);
+
+        //    connection.Close();
+        //}
+
         [TestMethod]
-        public void ConnectionString()
+        public void TestExecuteNonQuery()
         {
             Provider provider = new Provider();
 
-            Assert.AreEqual(provider.openConnection().ConnectionString, @"Server=.\SQLEXPRESS;Database=FashionShop;Integrated Security=SSPI;");
+            String sql = "Insert Into Manufacturer(ID, Name) Values ('abcxyz', 'OMO')";
+
+            bool result = provider.executeNonQuery(sql);
+
+            Assert.AreEqual(result, true);
+
+            if (result == true)
+            {
+                sql = "Delete from Manufacturer Where ID = 'abcxyz'";
+                provider.executeNonQuery(sql);
+            }
         }
 
         [TestMethod]
-        public void TestConnection()
+        public void TestExecuteQuery()
         {
             Provider provider = new Provider();
 
-            SqlConnection connection = provider.openConnection();
+            String sql = "Insert Into Manufacturer(ID, Name) Values ('abcxyz', 'OMO')";
+            provider.executeNonQuery(sql);
 
-            connection.Open();
+            sql = "Select * From Manufacturer Where ID = 'abcxyz'";
 
-            Assert.AreEqual(connection.State, ConnectionState.Open);
+            DataTable table = provider.executeQuery(sql);
+            int result = table.Rows.Count;
 
-            connection.Close();
+            Assert.AreEqual(result, 1);
+
+            if (result > 0)
+            {
+                sql = "Delete from Manufacturer Where ID = 'abcxyz'";
+                provider.executeNonQuery(sql);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetData()
+        {
+            Provider provider = new Provider();
+
+            String sql = "Insert Into Manufacturer(ID, Name) Values ('abcxyz0123', 'OMO')";
+            provider.executeNonQuery(sql);
+
+            sql = "Select * From Manufacturer Where ID = 'abcxyz0123'";
+
+            DataTable table = provider.executeQuery(sql);
+            DataRow row = table.Rows[0];
+
+            Assert.AreEqual(row.Field<string>(0), "abcxyz0123");
+
+            Assert.AreEqual(row.Field<string>(1), "OMO");
+
+            if (table.Rows.Count > 0)
+            {
+                sql = "Delete from Manufacturer Where ID = 'abcxyz'";
+                provider.executeNonQuery(sql);
+            }
         }
     }
 }
--- FashionShop/Models/Provider.cs
+++ FashionShop/Models/Provider.cs
@@ -3,21 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace FashionShop.Models
 {
+    /// <summary>
+    /// Support execute C.R.U.D with the database.
+    /// </summary>
     public class Provider
     {
+        /// <summary>
+        /// The connection
+        /// </summary>
         private SqlConnection connection;
 
-        public SqlConnection openConnection()
+        /// <summary>
+        /// Open connection to Microsoft SQL Server.
+        /// </summary>
+        /// <returns>SqlConnection conection</returns>
+        private SqlConnection openConnection()
         {
             if (this.connection == null)
             {
                 this.connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=FashionShop;Integrated Security=SSPI;");
             }
 
+            if (this.connection.State == ConnectionState.Closed)
+            {
+                this.connection.Open();
+            }
+
             return this.connection;
+        }
+
+        /// <summary>
+        /// Close connection.
+        /// </summary>
+        private void closeConnection()
+        {
+            try
+            {
+                this.connection.Close();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+            
+        }
+
+        /// <summary>
+        /// Execute Insert/Update/Delete.
+        /// </summary>
+        /// <param name="sql">T-SQL string</param>
+        /// <returns>bool isSuccessful</returns>
+        public bool executeNonQuery(string sql)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(sql, this.openConnection());
+                int state = command.ExecuteNonQuery();
+
+                if (state == 1)
+                {
+                    return true;
+                }
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+            finally
+            {
+                this.closeConnection();
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Get data from the database.
+        /// </summary>
+        /// <param name="sql">T-SQL string</param>
+        /// <returns>DataTable data from database</returns>
+        public DataTable executeQuery(string sql)
+        {
+            DataTable table = new DataTable();
+
+            try
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, this.openConnection());
+                adapter.FillSchema(table, SchemaType.Mapped);
+                adapter.Fill(table);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+            finally
+            {
+                this.closeConnection();
+            }
+
+            return table;
         }
     }
 }
